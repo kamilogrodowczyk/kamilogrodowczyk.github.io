c7015d82678d076934168f8199bf4312451fba6c
Garden <kamil.ogrodowczyk111@gmail.com>
2021-02-16 19:45:24 +0100

    Add a little bit animations to project

--- index.js
+++ index.js
@@ -10,3 +10,64 @@ const handleClick = () => {
 }
 
 hamburger.addEventListener('click', handleClick);
+
+const elements = document.querySelectorAll('.animate');
+    const options = {
+        root: null,
+        threshhold: 0.5,
+        rootMargin: '0px 0px -20% 0px'
+     };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                return;
+            }
+            entry.target.classList.add('animated');
+            observer.unobserve(entry.target)
+        })
+    }, options)
+
+    elements.forEach(element => observer.observe(element))
+
+const circle = document.querySelectorAll('.numbers__result');
+const circleOptions = {
+		root: null,
+		threshhold: 0.5,
+		rootMargin: '0px 0px -9% 0px'
+	};
+
+const observerCircle = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if(!entry.isIntersecting) {
+			return;
+		}
+		up()
+		observer.unobserve(entry.target)
+	})
+}, circleOptions)
+
+circle.forEach(element => observerCircle.observe(element))
+
+
+const counter = document.querySelectorAll('.numbers__result');
+const speed = 200;
+
+function up() {
+
+	counter.forEach(counter => {
+		function updateCount() {
+			const target = +counter.getAttribute('data-target');
+			const count = +counter.innerText
+			const inc = target / speed;
+			
+			if(count < target) {
+				counter.innerText = Math.ceil(count + inc);
+				setTimeout(updateCount, 1)
+			} else {
+				count.innerText = target;
+			}
+		}
+		updateCount();
+	})
+}
