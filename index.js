069af4a135518b0c468d89cd5be1975b43c9e21b
Garden <kamil.ogrodowczyk111@gmail.com>
2021-02-17 21:16:17 +0100

    v5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import hamburgerMenu from 'modules/hamburgerMenu.js';
-import observerAnimation from 'modules/observerAnimation.js';
-import observerCounter from 'modules/observerCounter.js';
-import stickyHeader from 'modules/stickyHeader.js';
+import hamburgerMenu from './modules/hamburgerMenu.js';
+import observerAnimation from './modules/observerAnimation.js';
+import observerCounter from './modules/observerCounter.js';
+import stickyHeader from './modules/stickyHeader.js';
 
 function init() {
 	hamburgerMenu();
