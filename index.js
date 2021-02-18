870c2538a88d0db92899a7216e8a19f0d072695a
Garden <kamil.ogrodowczyk111@gmail.com>
2021-02-18 11:50:31 +0100

    v15

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import hamburgerMenu from './modules/hamburgerMenu.js';
 import observerAnimation from './modules/observerAnimation.js';
 import observerCounter from './modules/observerCounter.js';
 import stickyHeader from './modules/stickyHeader.js';
+import mobileScreen from './modules/mobileScreen.js';
 
 function init() {
+	mobileScreen();
 	hamburgerMenu();
 	observerAnimation();
 	observerCounter();
