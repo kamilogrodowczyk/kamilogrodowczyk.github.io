0f4a48aff0b34923180a1fc932f5073eeef7e780
Garden <kamil.ogrodowczyk111@gmail.com>
2021-02-21 18:25:32 +0100

    v26

--- modules/stickyHeader.js
+++ modules/stickyHeader.js
@@ -5,9 +5,11 @@ export default function stickyHeader() {
         if(window.scrollY > header.offsetHeight) {
             navbar.classList.add('fixed');
             document.body.style.paddingTop = `${header.offsetHeight}`;
+            header.style.transform = 'translateY(99%)'
         } else {
             navbar.classList.remove('fixed')
             document.body.style.paddingTop = '0px';
+            header.style.transform = 'translateY(0%)'
         }
     }
     window.addEventListener('scroll', sticky)
