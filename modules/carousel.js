fe98d558b9c4ac4cef81f12cf1eb710753e867fa
Garden <kamil.ogrodowczyk111@gmail.com>
2021-02-26 10:25:10 +0100

    Add change for last slider image

--- modules/carousel.js
+++ modules/carousel.js
@@ -4,10 +4,10 @@ export default function carousel() {
 
     function handleClick() {        
         const target = this.getAttribute('aria-label')
-        const images = ['url(assets/slide1.jpg)', 'url(assets/slide2.jpg)', 'url(assets/slide3.jpg)'];
+        const images = ['url(assets/slide1.jpg)', 'url(assets/slide2.jpg)', 'url(assets/slide3.JPG)'];
 
         header.style.backgroundImage = images[target-1];
-        header.setAttribute('aria-label', `${target} of 4`);
+        header.setAttribute('aria-label', `${target} of 3`);
 
         button.forEach(element => {
             if(element.classList.contains('header__dot--active')) {
