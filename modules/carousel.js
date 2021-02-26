99d1949d2dba4bdda9ee4ce0833bdf14de2ea5c0
Garden <kamil.ogrodowczyk111@gmail.com>
2021-02-26 11:39:24 +0100

    Add two new icons for numbers section

--- modules/carousel.js
+++ modules/carousel.js
@@ -2,20 +2,18 @@ export default function carousel() {
     const button = document.querySelectorAll('.header__dot[aria-label]');
     const header = document.querySelector('.header__container[aria-label]');
 
-    function handleClick() {        
+    function handleClick(e) {        
         const target = this.getAttribute('aria-label')
-        const images = ['url(assets/slide1.jpg)', 'url(assets/slide2.jpg)', 'url(assets/slide3.JPG)'];
+        const images = ['', 'url(assets/slide1.jpg)', 'url(assets/slide2.jpg)', 'url(assets/slide3.JPG)'];
 
-        header.style.backgroundImage = images[target-1];
+        header.style.backgroundImage = images[target];
         header.setAttribute('aria-label', `${target} of 3`);
 
         button.forEach(element => {
             if(element.classList.contains('header__dot--active')) {
                 element.classList.remove('header__dot--active')
-            } else {
-                this.classList.add('header__dot--active')
             }
-            element.classList.remove('header__dot--active')
+            e.currentTarget.classList.add('header__dot--active')
         })
     }
 
