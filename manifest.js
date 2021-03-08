ebe25eda1361d1bce10e62b67c4d059b8077d243
Garden <kamil.ogrodowczyk111@gmail.com>
2021-03-08 15:13:07 +0100

    v49

--- manifest.js
+++ manifest.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('kamilogrodowczyk.github.io/sw.js', {scope: 'kamilogrodowczyk.github.io'})
+    navigator.serviceWorker.register('./sw.js')
     .then(registration => {
         console.log(registration)
     }).catch(error => {
