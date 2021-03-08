9bb82204b9ffdd6e4f104fd3f1b3d7b9b04e7321
Garden <kamil.ogrodowczyk111@gmail.com>
2021-03-08 15:24:26 +0100

    v54

--- sw.js
+++ sw.js
@@ -6,8 +6,7 @@
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open('static').then(cache => {
-            // return cache.addAll(["./index.html", "./css/home/", "./assets/logo-manifest512.png"])
-            console.log(cache)
+            return cache.addAll(["/"])
         })
     );
 })
